0863bbf36c231cf87d2de869e1a79341e779a10a
kunal dangi <105959379+kunal0157@users.noreply.github.com>
2024-05-17 15:53:14 +0530

    Initial commit

---
+++ src/data/constants.js
@@ -0,0 +1,362 @@
+export const Bio = {
+  name: "DANGI KUNAL",
+  roles: ["Full Stack Developer", "Web Developer", "Programmer"],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/kunal0157",
+  resume:
+    "https://drive.google.com/file/d/1kx3shASwml_JD-AIH0FAz0LWWiR-L3Ay/view?usp=drive_link",
+  linkedin: "https://www.linkedin.com/in/dangi-kunal-60a811227/",
+  twitter: "https://twitter.com/dangi_kuna50900",
+  insta: "https://www.instagram.com/kunal_dangi12/",
+  facebook: "https://www.facebook.com/profile.php?id=100015899238196",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Tailwind css",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDJp_n-r49IxsRWP8zaL7VA-KyDo-zLjpRcQem7qO-sQ&s",
+      },
+      {
+        name: "Next Js",
+        image:
+          "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACTklEQVR4Ab1XAaQqURB9DyohSykREpRIQSAlBCoECKUFCSRCBBEAaSEABQEoCIEASCwAUICALgCo83do0//9v819XX845O7VnDkzOzP7JWGaBd3C3IJpQVjAHeJ+Rs9a97vKLGrBsB1KgMhEP3FMUUwt4ENMfxr1yQIU4SSjRkbeOZtERmHk6pXQVDlnkHh9S+QLTm1hkiz4n/gzFQuny9FoFLquE+i34x+n02k0m00UCoV3BIzn3MMJrVYLtp1OJ0cS/X4f5/MZhmG8IyDsWtDfEaDIn2232/3zbrvdxuFwwGg04qRBt+VnETBNE0IIkE2n07/erdfrWK/X6Ha73Hb9ZXII3G43ivy3dNRqtZe7lUoFs9mM6oBDwCQCgquALT1FT3a5XF7qIZ/PYzgcolqtcggIIgBZAgRKB6lCRalp2uM8k8mAVMrlchwC+DEBipycE4n5fP44j8ViKJVKSCaTbAJCpgaez4vFIsjoWa/XA50FAgEkEgmEw2F2CkxZBZ5Br5tt1ITcbjd8Ph88Hg+7CBefECCsVitS4aVJcV9D/VMCVITk/Hq9YrPZyBBo2a1YMGvAcQYcj0cCtWMugcdYNhjDiBrP25mx3++x3W6RzWZZ8isfxzQLlsslJpMJpYY5jhkqcOH1ejEYDDAej9FoNOByuZxGsfqVzC7KTqcDSkkqleKsZOqX0mAwiHK5DGrJfr+fs5SqX8sjkQji8ThCoRC+v78Za7l6JagrUh3YkUuZpqgwDaecc9VYSDoV5Fg+at7n+eLN57kuE/EvzHr/Kvs31aYAAAAASUVORK5CYII=",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "C++",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/800px-ISO_C%2B%2B_Logo.svg.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+    ],
+  },
+  {
+    title: "Android",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      /*
+      {
+        name: "Git",
+        image:
+          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      },*/
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Git",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Git-logo.svg/225px-Git-logo.svg.png",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://i.pinimg.com/564x/af/70/80/af70800826083538b7cfab54a98c55f1.jpg",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://i.pinimg.com/564x/63/a7/b1/63a7b1894b3210d07e434e9d12170586.jpg",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://media.licdn.com/dms/image/C4E0BAQE_BsV_JiNDZw/company-logo_200_200/0/1632647499956/nrcrew_logo?e=2147483647&v=beta&t=lLQVE4TcFNbvT5gO4CVlEWCNLeFvLbsEvMYWAQWKgr0",
+    role: "Web developer intern",
+    company: "NR CREW",
+    date: "June 2024 - Present",
+    desc: "Working on Live Project,and learn reactjs and NextJs",
+    skills: [
+      "React",
+      "JavaScript",
+      "tailwind",
+      "TypeScript",
+      "Node Js",
+      "Next Js",
+      "Mongo db",
+    ],
+
+    doc: "",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://yt3.googleusercontent.com/ytc/AIdro_n1ksluIgtWVC879FrN7WMv_50OmRUawmiTspZ02pbmNm6e=s900-c-k-c0x00ffffff-no-rj",
+    school: "Silver Oak University,Ahmedabad ",
+    date: "Oct 2021 - May 2024",
+    grade: "8.71 CGPA",
+    desc: "I am complete Bca at Silver Oak University, Ahmedabad. I have completed 6 semesters and have last sem CGPA is 8.71. I have taken courses in  Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+    degree: "Bachelor of Computer Application - Bca",
+  },
+  {
+    id: 1,
+    img: "",
+    school: "Ashish Hindi School",
+    date: "Apr 2021",
+    grade: "65%",
+    desc: "I completed my class 12 high school education at Ashish Hindi School,Ahmedabad, where I studied Commerce",
+    degree: "GSEB(XII)",
+  },
+  {
+    id: 2,
+    img: "",
+    school: "Pushpanjali Vidhyalaya",
+    date: "Apr 2019",
+    grade: "63%",
+    desc: "I completed my class 10 education at Pushpanjali Vidhyalaya,Ahmedabad.",
+    degree: "GSEB(X)",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Admin Dashboard",
+    date: "june 2023 - may 2024",
+    description:
+      "Admin Dashboard is a dashboard for Admin. in this dashboard all the e-commerce site data and information are saved.and also graph are genrated.",
+    image:
+      "https://github.com/kunal0157/Admin-Dashboard-typescript/blob/main/src/assets/images/dash.png?raw=true",
+
+    tags: ["React js", "TypeScript", "scss"],
+    category: "web app",
+    github: "https://github.com/kunal0157/Admin-Dashboard-typescript",
+    webapp: "",
+    member: [
+      {
+        name: "Dangi Kunal",
+        img: "https://avatars.githubusercontent.com/u/105959379?s=400&u=831423053e1b1bea46709f93aaf747551e4c86d5&v=4",
+        linkedin: "https://www.linkedin.com/in/dangi-kunal-60a811227/",
+        github: "https://github.com/kunal0157",
+      },
+    ],
+  },
+  {
+    id: 8,
+    title: "E-commerce Full Stack Website",
+    date: "Jan 2024 - may 2024",
+    description:
+      "E-commerce is a website for clothes shop.it's my final year project.this project is full -stack website. it's make in nextjs.",
+    image:
+      "https://github.com/kunal0157/E-commerce-Full-Stack-Website/blob/main/src/assest/Screenshot%202024-03-26%20165959.png?raw=true",
+    tags: [
+      "React Js",
+      "Tailwind Css",
+      "Nextjs",
+      "nodejs",
+      "mongodb",
+      "firebase",
+    ],
+    category: "web app",
+    github: "https://github.com/kunal0157/E-commerce-Full-Stack-Website",
+    webapp: "https://e-commerce-full-stack-website-nine.vercel.app/",
+    member: [
+      {
+        name: "Dangi Kunal",
+        img: "https://avatars.githubusercontent.com/u/105959379?s=400&u=831423053e1b1bea46709f93aaf747551e4c86d5&v=4",
+        linkedin: "https://www.linkedin.com/in/dangi-kunal-60a811227/",
+        github: "https://github.com/kunal0157",
+      },
+    ],
+  },
+  {
+    id: 0,
+    title: "Currency Converter",
+    date: "June 2024 - may 2024",
+    description:
+      "Currency Converter is made in react js. in this project we can convert any currency to any country currency.",
+    image:
+      "https://github.com/kunal0157/currency-converter/blob/main/src/assets/Screenshot%202024-05-17%20110100.png?raw=true",
+    tags: ["React Js", "Tailwind Css"],
+    category: "web app",
+    github: "https://github.com/kunal0157/currency-converter",
+    webapp: "https://currency-converter-kappa-one.vercel.app/",
+    member: [
+      {
+        name: "Dangi Kunal",
+        img: "https://avatars.githubusercontent.com/u/105959379?s=400&u=831423053e1b1bea46709f93aaf747551e4c86d5&v=4",
+        linkedin: "https://www.linkedin.com/in/dangi-kunal-60a811227/",
+        github: "https://github.com/kunal0157",
+      },
+    ],
+  },
+  {
+    id: 1,
+    title: "Password Manager",
+    date: "June 2024 - may 2024",
+    description:
+      "password manager is made in react and tailwind. it use for save all my password in any social media platform and other platform.",
+    image:
+      "https://github.com/kunal0157/Password-manager/blob/main/src/assets/Screenshot%202024-05-17%20110159.png?raw=true",
+    tags: ["React Js", "Tailwind"],
+    category: "web app",
+    github: "https://github.com/kunal0157/Password-manager",
+    webapp: "https://password-manager-sepia.vercel.app/",
+    member: [
+      {
+        name: "Dangi Kunal",
+        img: "https://avatars.githubusercontent.com/u/105959379?s=400&u=831423053e1b1bea46709f93aaf747551e4c86d5&v=4",
+        linkedin: "https://www.linkedin.com/in/dangi-kunal-60a811227/",
+        github: "https://github.com/kunal0157",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Password Genrater",
+    date: "June 2024 - may 2024",
+    description:
+      "password Genrater is web app platform, in this web can genrate all type of password mix number,mix alphabet.",
+    image:
+      "https://github.com/kunal0157/Password-Genrater/blob/main/src/assets/Screenshot%202024-05-17%20110245.png?raw=true",
+    tags: ["React Js", "Tailwind"],
+    category: "web app",
+    github: "https://github.com/kunal0157/Password-genrater",
+    webapp: "https://password-genrater-six.vercel.app/",
+    member: [
+      {
+        name: "Dangi Kunal",
+        img: "https://avatars.githubusercontent.com/u/105959379?s=400&u=831423053e1b1bea46709f93aaf747551e4c86d5&v=4",
+        linkedin: "https://www.linkedin.com/in/dangi-kunal-60a811227/",
+        github: "https://github.com/kunal0157",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Yatharth Finance Cunsultonsy",
+    date: "June 2024 - may 2024",
+    description:
+      "Yathath Finance Cunsultoncy is a company website. it's my first live project. in this website user can see all insurance services and fill form to but insurance.",
+    image:
+      "https://github.com/kunal0157/Yatharth-finance/blob/main/img/das.png?raw=true",
+    tags: ["Html", "Css", "javaScript", "NodeMailer"],
+    category: "web app",
+    github: "https://github.com/kunal0157/Yatharth-finance",
+    webapp: "https://yatharth-finance.vercel.app/",
+    member: [
+      {
+        name: "Dangi Kunal",
+        img: "https://avatars.githubusercontent.com/u/105959379?s=400&u=831423053e1b1bea46709f93aaf747551e4c86d5&v=4",
+        linkedin: "https://www.linkedin.com/in/dangi-kunal-60a811227/",
+        github: "https://github.com/kunal0157",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "React-Router-Web",
+    date: "June 2024 - may 2024",
+    description: "this is normal website for practice React-Router-web.",
+    image:
+      "https://github.com/kunal0157/react-router-web/blob/main/src/assets/Screenshot%202024-05-17%20110524.png?raw=true",
+    tags: ["Html", "Css", "javaScript", "NodeMailer"],
+    category: "web app",
+    github: "https://github.com/kunal0157/react-router-web",
+    webapp: "https://react-router-web-sepia.vercel.app/",
+    member: [
+      {
+        name: "Dangi Kunal",
+        img: "https://avatars.githubusercontent.com/u/105959379?s=400&u=831423053e1b1bea46709f93aaf747551e4c86d5&v=4",
+        linkedin: "https://www.linkedin.com/in/dangi-kunal-60a811227/",
+        github: "https://github.com/kunal0157",
+      },
+    ],
+  },
+  {
+    id: 6,
+    title: "Theme-Switcher",
+    date: "June 2024 - may 2024",
+    description:
+      "Theme Switcher is normal card componant in this project you can change theme light to dark.",
+    image:
+      "https://github.com/kunal0157/Theme-switcher/blob/main/src/assets/Screenshot%202024-05-17%20110406.png?raw=true",
+    tags: ["Html", "Css", "javaScript", "NodeMailer"],
+    category: "web app",
+    github: "https://github.com/kunal0157/Theme-switcher",
+    webapp: "https://theme-switcher-kappa-rust.vercel.app/",
+    member: [
+      {
+        name: "Dangi Kunal",
+        img: "https://avatars.githubusercontent.com/u/105959379?s=400&u=831423053e1b1bea46709f93aaf747551e4c86d5&v=4",
+        linkedin: "https://www.linkedin.com/in/dangi-kunal-60a811227/",
+        github: "https://github.com/kunal0157",
+      },
+    ],
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
